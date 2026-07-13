60d1902ecd007f7185cf613d5e17060bf1a000de
X. Wang <xuyi@tetra.nl>
2026-07-14 00:36:33 +0200

    Fix quality issues in UartPeripheral and its tests

--- Simulator/src/jCPU/arm/UartPeripheral.java
+++ Simulator/src/jCPU/arm/UartPeripheral.java
@@ -3,13 +3,18 @@ package jCPU.arm;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.InputStream;
 
 public class UartPeripheral implements Peripheral {
     private final ConcurrentLinkedQueue<Byte> inputBuffer = new ConcurrentLinkedQueue<>();
 
     public UartPeripheral() {
+        this(System.in);
+    }
+
+    public UartPeripheral(InputStream in) {
         Thread readerThread = new Thread(() -> {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
                 while (true) {
                     int c = reader.read();
                     if (c != -1) {
@@ -17,7 +22,7 @@ public class UartPeripheral implements Peripheral {
                     }
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                System.err.println("UART reader error: " + e.getMessage());
             }
         });
         readerThread.setDaemon(true);
@@ -26,12 +31,14 @@ public class UartPeripheral implements Peripheral {
 
     @Override
     public byte read(int offset) {
+        // offset is ignored in this simple UART implementation
         Byte b = inputBuffer.poll();
         return b == null ? 0 : b;
     }
 
     @Override
     public void write(int offset, byte value) {
+        // offset is ignored in this simple UART implementation
         System.out.print((char) value);
     }
 }
--- Simulator/test/jCPU/arm/UartPeripheralTest.java
+++ Simulator/test/jCPU/arm/UartPeripheralTest.java
@@ -2,21 +2,46 @@ package jCPU.arm;
 
 import static org.junit.Assert.*;
 import org.junit.*;
-import jCPU.arm.UartPeripheral;
-import jCPU.arm.Peripheral;
+import java.io.*;
 
 public class UartPeripheralTest {
+    private final PrintStream originalOut = System.out;
+    private final InputStream originalIn = System.in;
+    private ByteArrayOutputStream testOut;
+
+    @Before
+    public void setUp() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
+        System.setIn(originalIn);
+    }
+
     @Test
     public void testWrite() {
         Peripheral uart = new UartPeripheral();
         uart.write(0, (byte) 'H');
         uart.write(0, (byte) 'i');
+        assertEquals("Hi", testOut.toString());
     }
 
     @Test
     public void testRead() {
-        Peripheral uart = new UartPeripheral();
-        byte b = uart.read(0);
-        // Should not crash, might be 0 if no input
+        String input = "Hello";
+        Peripheral uart = new UartPeripheral(new ByteArrayInputStream(input.getBytes()));
+        
+        // Give the reader thread a moment to process the input
+        try { Thread.sleep(100); } catch (InterruptedException e) {}
+        
+        assertEquals((byte) 'H', uart.read(0));
+        assertEquals((byte) 'e', uart.read(0));
+        assertEquals((byte) 'l', uart.read(0));
+        assertEquals((byte) 'l', uart.read(0));
+        assertEquals((byte) 'o', uart.read(0));
+        assertEquals((byte) 0, uart.read(0));
     }
 }
