18135bc85cc6951e928f4f74a1036335e587de56
X. Wang <xuyi@tetra.nl>
2026-08-07 17:18:10 +0200

    docs(verifier): document LocalVarsStrategy contracts

--- ifOS/src/jx/zero/verifier/LocalVarsStrategy.java
+++ ifOS/src/jx/zero/verifier/LocalVarsStrategy.java
@@ -11,7 +11,20 @@ import jx.zero.verifier.npa.NPAValue;
 public interface LocalVarsStrategy {
     void write(int index, NPAValue type, int bcAddr);
 
+    /**
+     * Reads the value of the local variable at {@code index}.
+     *
+     * <p>Behavior for an uninitialized slot is implementation-specific:
+     * implementations may return a default {@code NPAValue} or throw an
+     * unchecked exception.
+     */
     NPAValue read(int index);
 
+    /**
+     * Changes the value of every local variable sharing the same id as
+     * {@code value} to {@code newVal}.
+     *
+     * <p>{@code value} must carry a valid id.
+     */
     void setValue(NPAValue value, int newVal);
 }
